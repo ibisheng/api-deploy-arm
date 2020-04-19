4211bd06df5cfdd9b987365635250befc1e5931f
xiedan <xiedan@gmail.com>
2020-04-19 19:46:18 +0800

    更新文档

--- preinstall-ubuntu.sh
+++ preinstall-ubuntu.sh
@@ -8,7 +8,7 @@ apt-get -y \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=arm64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 apt-get -y  update
