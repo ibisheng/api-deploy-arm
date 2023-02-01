fbf4dbbae64b3703ea60291c4e8c5c05700bab0c
毕升Office <xiehongan123@163.com>
2023-02-01 10:35:43 +0000

    update pullImage.sh.
    Signed-off-by: 毕升Office <xiehongan123@163.com>

--- pullImage.sh
+++ pullImage.sh
@@ -5,5 +5,5 @@ docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/editor_arm:$1
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/tools_arm:$1
 
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/mongo_arm:3.6
-docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/redis_arm:4.0
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/redis_arm:6.2.8
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/minio_arm:latest
